1e3866948b6b30c38fc9b2956af58d9738d0b26b
test0987654321234567890 <141794311+test0987654321234567890@users.noreply.github.com>
2023-12-26 17:02:15 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,8 +1,11 @@
 async function handler(request, connInfo) {
   const requrl=new URL(request.url)
   if(requrl.pathname=="/make"){
-    const html=Deno.readTextFile("./site/index.html")
+    const html=await Deno.readTextFile("./site/index.html")
     return new Response(html)
-  }else if(requrl.pathname=="/url"){}
+  }else if(requrl.pathname=="/url"){
+    
+  }
+  return new Response("",{status:404})
 }
 Deno.serve(handler)
