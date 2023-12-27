cba29d34444317aaa3fb8e1a7cf8f9c9828520e8
test0987654321234567890 <141794311+test0987654321234567890@users.noreply.github.com>
2023-12-27 11:44:06 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -15,6 +15,10 @@ async function handler(request, connInfo) {
       let dummyHTML=await Deno.readTextFile("./server/dummy.html")
       dummyHTML=dummyHTML.replaceAll("{img}",img).replaceAll("{x}",x).replaceAll("{y}",y).replaceAll("{text}",text).replaceAll("{title}",title)
       return new Response(dummyHTML,{status:200,headers:{"Content-Type":"text/html;charset=UTF-8"}})
+    }else if(requrl.pathname=="/img"){
+      let id=decodeURIComponent(requrl.searchParams.get("id"))
+      let res=await fetch("https://api-data.line.me/v2/bot/message/"+id+"/content",{headers:{"Authorization": `Bearer EVRl5WucxFH1XkeEnds0sd9lXj2Lli+LMpmVbmH+rV2MtCtOs/OxpwPT20qJ4eTT1PNRbOOxT/c3v7OJUBgmaU2i9HrzzgllPCTe84NbekzHCENGOes95u0OqnnJrywqnyROAKlcvr3qtU0wAfG4fAdB04t89/1O/w1cDnyilFU=`}})
+      return res
     }else{
       let url=decodeURIComponent(requrl.searchParams.get("url"))
       return new Response("",{status:301,headers:{"location":url}})
